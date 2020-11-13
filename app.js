7579cadd10096f67b3dfb09e2e024a4df5950adb
Sergiy <sergiybryukhovych@gmail.com>
2020-11-14 01:46:04 +0200

    File copy and rename
    Copy to the temp folder and rename file

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-console.log('main app')
+console.log('----------main app----------')
 
 const zip = require('decompress')
 const fs = require('fs')
@@ -6,11 +6,22 @@ const path = require('path')
 
 // Taking all zip and rename it
 
-
+fs.readdir('./original_zips', (err, data) =>{
+    console.log(data);
+    data.forEach((file, i) => {
+        console.log(file + ' ' + path.extname(file))
+        let ext = path.extname(file)
+        if (ext == '.zip'){
+            fs.copyFileSync('./original_zips/'+ file, './temp/'+ i + '.zip')
+            console.log('The copy has been made')
+            console.log('New name is ' + i + '.zip')
+        }
+    })
+})
 
 // Unzip - This part works well
 
-zip('original_zips/1.zip', 'original_zips').then(files => {
-    console.log('done!');
-});
+// zip('original_zips/1.zip', 'original_zips').then(files => {
+//     console.log('done!');
+// });
 
