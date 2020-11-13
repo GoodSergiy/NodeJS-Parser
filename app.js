37e55598df83e3c24cdfe0c67fa87cab6adfc86c
Sergiy <sergiybryukhovych@gmail.com>
2020-11-14 00:28:53 +0200

    First unzip
    First working Unzipper

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
 console.log('main app')
+
+const zip = require('decompress')
+const fs = require('fs')
+const path = require('path')
+
+// Taking all zip and rename it
+
+
+
+// Unzip - This part works well
+
+zip('original_zips/1.zip', 'original_zips').then(files => {
+    console.log('done!');
+});
+
