a56d40d898c9921c0d2d6500cb627f8aa50547ea
Sergiy <sergiybryukhovych@gmail.com>
2020-11-15 17:30:21 +0200

    Almost done corrections

--- CSVtoJSON.js
+++ CSVtoJSON.js
@@ -4,6 +4,8 @@ const csv = require('csv-parser')
 const es = require('event-stream')
 const rimraf = require("rimraf")
 
+// Wright to JSON
+
 function cjs(){
   fs.mkdir('OutputJSON', (err) => {
     if (err) console.log('OutputJSON exist')
@@ -38,32 +40,11 @@ function cjs(){
       }
   },100)
   
+// Delete unziped CSV
+
   setTimeout(() => {
     rimraf('./CSV', () => { console.log('CSV removed'); console.log('The process is DONE!') });    
   },500)
 }
 
 module.exports.cjs = cjs;
-
-
-// Wright to JSON ###### - This part works well
-// const data = [];
-
-// fs.writeFile('./OutputJSON/data.json', JSON.stringify(data), (err) => {
-//   if (err) console.log('WRONG :(')
-// })
-
-// Read CSV and Write to the JSON ###### - This part works 
-
-// const results = new Array();
-
-// fs.createReadStream('./CSV/users-test.csv')
-//     .pipe(es.split('"'))
-//     .pipe(csv({separator: '||'}))  // handle CSV with | separator
-//     .on('data', (data) => results.push(data))
-//     .on('end', () => {      
-//       console.log(results);
-//       fs.writeFileSync('./OutputJSON/data.json', JSON.stringify(results),'utf-8', (err) => {
-//           if (err) console.log('That is bad')
-//       })
-//   })
--- app.js
+++ app.js
@@ -1,9 +1,5 @@
 console.log('----------main app----------')
 
-// const zip = require('decompress')
-// const fs = require('fs')
-// const path = require('path')
-// const del = require("rimraf")
 const unzip = require('./unzip.js')
 const csv = require('./CSVtoJSON')
 
